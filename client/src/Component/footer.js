b89eeab0c7f90e292c9a8fbc4e168dd7d24db7b7
Teja8845 <luckyteja31@gmail.com>
2022-10-11 17:26:31 +0530

    update

---
+++ client/src/Component/footer.js
@@ -0,0 +1,13 @@
+export default function footer() {
+    return (
+        <footer class="site-footer">
+            <div class="container">
+                
+        
+                <div class="colophon">Developed for Mr Cooper</div>
+                
+            </div>
+
+        </footer>
+    )
+}
